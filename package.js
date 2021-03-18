6aee900d5be5287d0178b9b029803d9f18ac6fa5
maxwynne <76392789+maxwynne@users.noreply.github.com>
2021-03-18 18:27:59 +0000

    Create package.js

---
+++ package.js
@@ -0,0 +1,28 @@
+{
+  "name": "fcbot",
+  "version": "1.0.0",
+  "description": "",
+  "main": "bot.js",
+  "dependencies": {
+    "bufferutil": "^3.0.5",
+    "discord.js": "^11.4.2",
+    "fs": "^0.0.1-security",
+    "libsodium-wrappers": "^0.7.3",
+    "moment": "^2.22.2",
+    "mssql": "^4.2.2",
+    "mysql": "^2.16.0",
+    "opusscript": "^0.0.6",
+    "path": "^0.12.7",
+    "request": "^2.88.0",
+    "uws": "^9.148.0"
+  },
+  "devDependencies": {
+    "mocha": "^5.2.0"
+  },
+  "scripts": {
+    "test": "node test.js",
+    "start": "node bot.js"
+  },
+  "author": "Roman Petrov",
+  "license": "ISC"
+}
